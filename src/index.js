8770cd510df21b00009c1a00734831998dd78b69
brquispe <BraianRafael.Quispe@alumnos.uai.edu.ar>
2022-11-09 00:18:27 -0300

    Add test endpoint

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ app.get('', (req, res) => {
   res.json({ version: "1.0.0" });
 })
 
+app.get('api/', (req, res) => {
+  res.json({ message: "prueba" })
+})
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening at http://localhost:${process.env.PORT}`)
 })
